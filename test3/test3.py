6ba5bda209158734ce8bf2fc2b9b289139113182
Flor Vela <flor.p.vela@gmail.com>
2018-10-19 10:11:26 -0300

    Adding tests 3 and 4

---
+++ test3/test3.py
@@ -0,0 +1,17 @@
+import cv2
+
+img = cv2.imread('scene.jpg',0)
+
+# Initiate ORB detector
+orb = cv2.ORB_create()
+
+# find the keypoints with ORB
+kp = orb.detect(img, None)
+
+# compute the descriptors with ORB
+kp, des = orb.compute(img, kp)
+
+# draw only keypoints location,not size and orientation
+img2 = cv2.drawKeypoints(img, kp, img, color=(0,255,0), flags=0)
+cv2.imshow('keypoints',img2)
+cv2.waitKey(0)
